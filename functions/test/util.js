089d45897270180d91a83c49fcc2f8bfd6b5f940
T-Aruga <56016020+T-Aruga@users.noreply.github.com>
2020-10-19 01:46:15 +0900

    Add batch delete

---
+++ functions/test/util.js
@@ -0,0 +1,55 @@
+const admin = require('firebase-admin');
+admin.initializeApp({
+    credential: admin.credential.cert("./test/logtest-demo-firebase-adminsdk-b246u-1df060c162.json"),
+    databaseURL: 'https://logtest-demo.firebaseio.com',
+});
+const db = admin.firestore();
+
+//firebaseのサイトにあるコード（少し改修）
+const deleteCollection = (db, collectionRef, batchSize) => {
+  const query = collectionRef.orderBy('__name__').limit(batchSize);
+  return new Promise((resolve, reject) => {
+      deleteQueryBatch(db, query, batchSize, resolve, reject);
+  });
+}
+
+//削除のメインコード
+const deleteQueryBatch = (db, query, batchSize, resolve, reject) => {
+  query.get()
+      .then((snapshot) => {
+
+           //検索結果が0件なら処理終わり
+          if (snapshot.size == 0) {
+              return 0;
+          }
+
+           //削除のメイン処理
+          const batch = db.batch();
+          snapshot.docs.forEach(doc => {
+              batch.delete(doc.ref);
+          });
+
+           //一旦処理サイズをreturn
+          return batch.commit().then(() => {
+              return snapshot.size;
+          })
+      })
+      .then((numDeleted) => {
+
+           //もう対象のデータが0なら処理終わり
+          if (numDeleted == 0) {
+              resolve();
+              return;
+          }
+
+           //あだあるなら、再度処理
+          process.nextTick(() => {
+              deleteQueryBatch(db, query, batchSize, resolve, reject);
+          });
+      })
+      .catch(reject);
+}
+
+//実行
+// const colRef = db.collection("members");
+deleteCollection(db, colRef, 500);
