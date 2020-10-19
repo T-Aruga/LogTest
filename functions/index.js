a6bc461c8b2d7f5a1d3ee986230b5b679a52a8a9
T-Aruga <56016020+T-Aruga@users.noreply.github.com>
2020-10-20 02:38:17 +0900

    Add firebase config

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,6 @@
 const functions = require('firebase-functions');
 const f = functions.region('asia-northeast1');
 const Translator = require('./Translator');
-// const admin = require('firebase-admin');
-// admin.initializeApp();
-// const db = admin.firestore();
-// const FireStoreClient = require('./FireStoreClient');
-// const timestamp = admin.firestore.FieldValue.serverTimestamp();
 
 
 module.exports.translate = f.https.onRequest(async (req, res) => {
@@ -24,15 +19,3 @@ module.exports.translate = f.https.onRequest(async (req, res) => {
     res.status(500).json({ "error": error.toString() });
   } 
 });
-
-
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Cloud Firestore under the path /messages/:documentId/original
-// module.exports.addMessage = f.https.onRequest(async (req, res) => {
-//   // Grab the text parameter.
-//   const original = req.query.text;
-//   // Push the new message into Cloud Firestore using the Firebase Admin SDK.
-//   const writeResult = await admin.firestore().collection('messages').add({ original: original });
-//   // Send back a message that we've succesfully written the message
-//   res.status(200).json({result: `Message ${original} added.`});
-// });
