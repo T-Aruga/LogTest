972000e62bcad9f49d374a56bce9f74f4cdfe3ec
T-Aruga <56016020+T-Aruga@users.noreply.github.com>
2020-10-27 11:02:55 +0900

    Add express

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,26 @@
+const express = require("express");
+const app = express();
 const functions = require('firebase-functions');
 const f = functions.region('asia-northeast1');
-const cors = require('cors')({ origin: true });
 const Translator = require('./Translator');
+const allowedCrossOrigin = ['http://localhost:3000', 'https://console.roboticcrowd.com', 'https://console.devk8s.com'];
 
-
-
-module.exports.translate = f.https.onRequest(async (req, res) => {
-  // cors対策
-  const host = req['headers']['x-original-host'];
-  console.log(req['headers']['x-original-host']);
-  if (host === 'localhost') {
-    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
-  } else if (host === 'console.roboticcrowd.com' || host === 'console.devk8s.com') {
-    res.set('Access-Control-Allow-Origin', 'https://' + host);
+app.use(function (req, res, next) {
+  const origin = req.headers.origin;
+  if (allowedCrossOrigin.indexOf(origin) > -1) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
   }
-  res.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST');
-  if (req.method == "OPTIONS") {
-    cors(req, res, () => {
-      res.status(200).send()
-    });
-    return
-  }
+  next();
+});
+
+app.options('*', function (req, res) {
+  res.sendStatus(200);
+});
+
+
+app.post('/', async (req, res) => { 
   const body = req.body;
   const errorMsg = body.body;
   const locale = body.locale;
@@ -36,3 +36,38 @@ module.exports.translate = f.https.onRequest(async (req, res) => {
     res.status(500).json({ "error": error.toString() });
   }
 });
+
+const translate = f.https.onRequest(app);
+module.exports = { translate };
+
+// module.exports.translate = f.https.onRequest(async (req, res) => {
+//   // cors対策
+//   const host = req['headers']['x-original-host'];
+//   console.log(req['headers']['x-original-host']);
+//   if (host === 'localhost') {
+//     res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   } else if (host === 'console.roboticcrowd.com' || host === 'console.devk8s.com') {
+//     res.set('Access-Control-Allow-Origin', 'https://' + host);
+//   }
+//   res.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST');
+//   if (req.method == "OPTIONS") {
+//     cors(req, res, () => {
+//       res.status(200).send()
+//     });
+//     return
+//   }
+//   const body = req.body;
+//   const errorMsg = body.body;
+//   const locale = body.locale;
+//   if (!errorMsg || !locale) {
+//     return res.status(400).json({ "error": "body and locale are mandatory." });
+//   }
+//   const translator = new Translator(errorMsg, locale);
+//   try {
+//     const response = await translator.translate();
+//     res.status(200).json(response);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ "error": error.toString() });
+//   }
+// });
